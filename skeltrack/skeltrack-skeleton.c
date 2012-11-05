2ded63929222ffaf02cc1b3421c41dbf557bb6bd
Iago López Galeiras <iaguis@gmail.com>
2012-11-05 13:07:35 +0100

    Fix inefficient list traversal

--- skeltrack/skeltrack-skeleton.c
+++ skeltrack/skeltrack-skeleton.c
@@ -697,11 +697,12 @@ GList *
 make_graph (SkeltrackSkeleton *self, GList **label_list)
 {
   SkeltrackSkeletonPrivate *priv;
-  gint i, j, n;
+  gint i, j;
   Node *node;
   GList *nodes = NULL;
   GList *labels = NULL;
   GList *current_label;
+  GList *current_node;
   Label *main_component_label = NULL;
   gint index = 0;
   gint next_label = -1;
@@ -813,9 +814,11 @@ make_graph (SkeltrackSkeleton *self, GList **label_list)
         }
     }
 
-  for (n = 0; n < g_list_length (nodes); n++)
+  for (current_node = g_list_first (nodes);
+       current_node != NULL;
+       current_node = g_list_next (current_node))
     {
-      Node *node = (Node *) g_list_nth_data (nodes, n);
+      Node *node = (Node *) current_node->data;
       node->label = label_find (node->label);
       node->label->nodes = g_list_prepend (node->label->nodes,
                                           node);
